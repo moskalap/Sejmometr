151f9884297e34fd1d045657ddbe61a3f031511b
moskalap <executeorder66>
2016-12-17 14:26:25 +0100

    dodano parlament i partie

--- src/Politican.java
+++ src/Politican.java
@@ -2,7 +2,8 @@
  * Created by przemek on 16.12.16.
  */
 public class Politican {
-    private id;
+
+    private int id;
     private String firstname;
     private String lastname;
 
@@ -13,13 +14,17 @@ public class Politican {
 
     }
 
+
     public boolean equals(Object other){
-
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Politican that = (Politican) other;
+        return this.id==that.id;
 
     }
 
-    public int hashCode(){
-        
-    }
 
+    public String getID() {
+        return new Integer(id).toString();
+    }
 }
